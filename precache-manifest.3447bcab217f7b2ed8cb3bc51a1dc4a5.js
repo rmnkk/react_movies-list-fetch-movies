27b7b9c933eb93a78d09c2b52960cc4ccb401827
Roman <romikkus@gmail.com>
2020-09-12 22:05:55 +0300

    Updates

---
+++ precache-manifest.3447bcab217f7b2ed8cb3bc51a1dc4a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368296a2d7e510cae5c8091a14e0f5ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c4d1de91e529b205cd4",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "bbc78824091441dcf09e",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "9c4d1de91e529b205cd4",
+    "url": "./static/js/2.99d85d3a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.99d85d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc78824091441dcf09e",
+    "url": "./static/js/main.1f29ecef.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
